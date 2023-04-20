822f72b9e45618443ee1a4d86c92b25faa956904
JangWheesung <gameplay1qaz@gmail.com>
2023-04-20 19:27:27 +0900

    TouchScreenClear

--- SheepBaby/Assets/02.Scripts/Whee/TouchScreen.cs
+++ SheepBaby/Assets/02.Scripts/Whee/TouchScreen.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class TouchScreen : MonoBehaviour
 {
-    public float sensitivity = 1f;      // 이동 감도
-
-    private Vector3 lastPosition;       // 마우스가 이동한 마지막 위치
+    public float sensitivity = 1f;
+    public float range = 5f;
+    private Vector3 lastPosition;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             lastPosition = Input.mousePosition;
-            Debug.Log(323);
         }
 
         if (Input.GetMouseButton(0))
         {
             Camera.main.ScreenToWorldPoint(lastPosition);
             Vector3 delta = Input.mousePosition - lastPosition;
-            transform.Translate(delta.x * sensitivity * Time.deltaTime, 0, 0);
-            lastPosition = Input.mousePosition;
+            Vector3 newPos = transform.position + new Vector3(delta.x * sensitivity * Time.deltaTime, 0, 0);
+
+            // 이동 가능 범위 제한
+            if (Mathf.Abs(newPos.x) < range)
+            {
+                transform.Translate(delta.x * sensitivity * Time.deltaTime, 0, 0);
+                lastPosition = Input.mousePosition;
+            }
         }
 
         if (Input.touchCount > 0)

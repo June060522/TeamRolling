6187bc5186fc19ec7fc9f61fdd2e4f94df5a0652
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-04-21 00:35:04 +0900

    4.21

--- SheepBaby/Assets/02.Scripts/HM/SoccerGame.cs
+++ SheepBaby/Assets/02.Scripts/HM/SoccerGame.cs
@@ -6,7 +6,8 @@ public class SoccerGame : MonoBehaviour
 {
     public float speed = 10f; // 축구공 이동 속도
     public Vector2 KeyInput;
-    public float currentPower;
+    private float MaxPower = 15, chargSpeed = 20;
+    private float CurrnetPower = 0;
 
     private Rigidbody2D rb;
 
@@ -24,17 +25,27 @@ public class SoccerGame : MonoBehaviour
     public void BallMove()
     {
         float moveHorizontal = Input.GetAxis("Horizontal"); // 좌우 이동
-        //float moveVertical = Input.GetAxis("Vertical"); // 앞뒤 이동
+        float moveVertical = Input.GetAxis("Vertical"); // 앞뒤 이동
 
         Vector3 movement = new Vector3(moveHorizontal, 0f, 0f); // 이동 벡터 생성
-        rb.AddForce(movement * speed); // 이동 속도에 따라 힘을 가해 축구공 이동
+        rb.velocity = new Vector3(moveHorizontal * speed, 0f, 0f);
     }
 
     private void Shoot()
     {
+        Fire();
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(KeyInput.normalized * currentPower, ForceMode2D.Impulse);
+            CurrnetPower += chargSpeed * Time.deltaTime;
+            CurrnetPower = Mathf.Clamp(CurrnetPower, 0, MaxPower);
         }
+        
+    }
+
+    private void Fire()
+    {
+        rb.velocity = Vector2.zero;
+        rb.AddForce(KeyInput.normalized * CurrnetPower, ForceMode2D.Impulse);
+        CurrnetPower = 0;
     }
 }

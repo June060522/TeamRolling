affd0f54eabdee2bf0265f89fcce2cf9a6083c2d
JangWheesung <gameplay1qaz@gmail.com>
2023-05-01 00:56:38 +0900

    LobbybUTTON

--- SheepBaby/Assets/02.Scripts/HM/GameStartManager.cs
+++ SheepBaby/Assets/02.Scripts/HM/GameStartManager.cs
@@ -15,37 +15,47 @@ public class GameStartManager : MonoBehaviour
     [SerializeField] GameObject sheepEmple;
     [SerializeField] private TextMeshProUGUI inputText;
 
-    public void SheepSetting()
+    void Awake()
+        => DontDestroyOnLoad(sheepEmple);
+
+    //public void SheepSetting()
+    //{
+    //    string s = inputText.text[0].ToString();
+
+    //    try
+    //    {
+    //        if (inputText.text.Length < 3)
+    //        {
+    //            SheepAbiliity[] oldSheep = FindObjectsOfType<SheepAbiliity>();
+    //            foreach (SheepAbiliity oSheep in oldSheep)
+    //                Destroy(oSheep.gameObject);
+
+    //            float pos = -(float.Parse(s) - 1) / 2;
+    //            for (float i = pos; i <= -pos; i += 1f)
+    //            {
+    //                GameObject newSheep = Instantiate(sheep, new Vector3(i, -0.02f, 0), Quaternion.identity);
+    //                newSheep.transform.parent = sheepEmple.transform;
+    //                newSheep.tag = "Sheep";
+    //            }
+    //        }
+    //    }
+    //    catch (Exception exp) { }
+    //}
+
+    //public void StartWheeSong()
+    //{
+    //    string s = inputText.text[0].ToString();
+
+    //    PlayerPrefs.SetFloat("SheepAmount", int.Parse(s));
+
+    //    if (int.Parse(s) > 0 && int.Parse(s) < 10 && inputText.text.Length < 3)
+    //        SceneManager.LoadScene("DevWheesung");
+    //}
+
+    public void InputSheep(GameObject sheep)
     {
-        string s = inputText.text[0].ToString();
-
-        try
-        {
-            if (inputText.text.Length < 3)
-            {
-                SheepAbiliity[] oldSheep = FindObjectsOfType<SheepAbiliity>();
-                foreach (SheepAbiliity oSheep in oldSheep)
-                    Destroy(oSheep.gameObject);
-
-                float pos = -(float.Parse(s) - 1) / 2;
-                for (float i = pos; i <= -pos; i += 1f)
-                {
-                    GameObject newSheep = Instantiate(sheep, new Vector3(i, -0.02f, 0), Quaternion.identity);
-                    newSheep.transform.parent = sheepEmple.transform;
-                    newSheep.tag = "Sheep";
-                }
-            }
-        }
-        catch (Exception exp) { }
-    }
-
-    public void StartWheeSong()
-    {
-        string s = inputText.text[0].ToString();
-
-        PlayerPrefs.SetFloat("SheepAmount", int.Parse(s));
-
-        if (int.Parse(s) > 0 && int.Parse(s) < 10 && inputText.text.Length < 3)
-            SceneManager.LoadScene("DevWheesung");
+        GameObject newSheep = Instantiate(sheep, new Vector3(0, -0.02f, 0), Quaternion.identity);
+        newSheep.transform.parent = sheepEmple.transform;
+        newSheep.tag = "Sheep";
     }
 }
--- SheepBaby/Assets/02.Scripts/Whee/MinigameManager.cs
+++ SheepBaby/Assets/02.Scripts/Whee/MinigameManager.cs
@@ -48,9 +48,12 @@ public class MinigameManager : MonoBehaviour
         {
             float plusValue = 0;
 
-            if (true)//조건문에 bool형식 미니게임 끝나는 함수 넣기(out float 메게변수 넣어 plusValue바꿔주고)
+            waterPanel.SetActive(true);
+
+            if (WaterMinigame.instance.EndGame(out plusValue))//조건문에 bool형식 미니게임 끝나는 함수 넣기(out float 메게변수 넣어 plusValue바꿔주고)
             {
                 sheepAbiliity.ChangeStat(Stat.hungry, plusValue);
+                waterPanel.SetActive(false);
                 end = true;
             }
             yield return new WaitForSeconds(0.2f);

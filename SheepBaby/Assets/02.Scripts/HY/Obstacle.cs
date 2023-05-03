a5ea87f0987d121b1c66edc9c1395aaa6487d407
hyeyeon059 <leehuijuan0912@gmail.com>
2023-05-04 01:50:40 +0900

    러닝 미니게임 버그 수정

--- SheepBaby/Assets/02.Scripts/HY/Obstacle.cs
+++ SheepBaby/Assets/02.Scripts/HY/Obstacle.cs
@@ -6,38 +6,25 @@ using TMPro;
 public class Obstacle : MonoBehaviour
 {
     [SerializeField] float speed;
-    [SerializeField] GameObject[] ground = new GameObject[2];
-    [SerializeField] GameObject[] obstacle = new GameObject[3];
+    [SerializeField] GameObject[] obstacle = new GameObject[2];
     [SerializeField] GameObject gameoverPanel;
     [SerializeField] TextMeshProUGUI scoreTxt;
 
-    public static bool isGame = true;
+    public bool isGame = true;
     int scoreCnt = 0;
 
     private void Update()
     {
         if (isGame)
         {
-            speed=3.5f;
+            speed = 2f;
 
-            scoreCnt += 1;
-            scoreTxt.text = "score: " + scoreCnt.ToString();
-
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 2; i++)
             {
                 Move(obstacle[i]);
                 if (obstacle[i].transform.position.x < -14)
                 {
                     obstacle[i].transform.position = new Vector3(7, obstacle[i].transform.position.y, 0);
-                }
-            }
-
-            for (int i = 0; i < 2; i++)
-            {
-                Move(ground[i]);
-                if (ground[i].transform.position.x <= -10)
-                {
-                    ground[i].transform.position = new Vector3(0, ground[i].transform.position.y, 0);
                 }
             }
         }
@@ -55,11 +42,11 @@ public class Obstacle : MonoBehaviour
         speed = 1;
         scoreCnt = 0;
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 2; i++)
         {
-            obstacle[i].transform.position = new Vector3(Random.Range(0,15),
+            obstacle[i].transform.position = new Vector3((i * 8) + i,
                 obstacle[i].transform.position.y, 0);
         }
-        gameoverPanel.SetActive(false);
+        gameoverPanel.gameObject.SetActive(false);
     }
 }
